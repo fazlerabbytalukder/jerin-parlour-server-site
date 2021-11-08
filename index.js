f1fb0b5d925019c45d6997ebd7d16b29ed93f219
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-11-08 23:57:03 +0600

    admiin and non admin

--- index.js
+++ index.js
@@ -92,6 +92,32 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.json(result);
         })
+        //MAKE ADMIN OR NORMAL USERS
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            // console.log(user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+        //DIFFERENTIATE ADMIN CAN ONLY ADD ADMIN
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user) {
+                if (user.role === 'admin') {
+                    isAdmin = true;
+                }
+                res.json({ admin: isAdmin });
+            }
+            else {
+                res.json({ admin: isAdmin });
+            }
+            // res.json('dd');
+        })
 
 
     } finally {
