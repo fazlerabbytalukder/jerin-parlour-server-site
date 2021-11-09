34dc13154820ffcceedac3e56be023f259141b16
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-11-09 10:18:45 +0600

    order delete

--- index.js
+++ index.js
@@ -149,6 +149,14 @@ async function run() {
             res.json(result)
         })
 
+        //DELETE booking data from all booking
+        app.delete('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id:ObjectId(id) };
+            const result = await BookingCollection.deleteOne(query);
+            res.json(result);
+        })
+
 
     } finally {
         // await client.close();
